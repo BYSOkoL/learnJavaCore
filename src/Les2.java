ffd7d59e30bc3d4d50f4860fd478e9cfc594961d
Dmitry Sokolov <131367242+BYSOkoL@users.noreply.github.com>
2023-04-26 20:36:20 +0300

    -out

--- src/Les2.java
+++ src/Les2.java
@@ -1,5 +1,37 @@
+import java.sql.SQLOutput;
+
 public class Les2 {
     public static void main(String[] args) {
+
+        String message = "Hello Java!";
+        System.out.println(message);
+
+        Boolean bool = false;
+        System.out.println(bool);
+
+        byte byteNumber = 10;
+        System.out.println(byteNumber);
+
+        short shortNumber = 1000;
+        System.out.println(shortNumber);
+
+        int intNumber = 123456;
+        System.out.println(intNumber);
+
+        long longNumber = 1231111111111L;
+        System.out.println(longNumber);
+
+        //char symbol = 'A';
+        char symbol = 65;
+        System.out.println(symbol);
+
+        float floatNumber = 10.5f;
+        System.out.println(floatNumber);
+
+        double doubleNumber = 10.5;
+        System.out.println(doubleNumber);
+
+
 
 
     }
