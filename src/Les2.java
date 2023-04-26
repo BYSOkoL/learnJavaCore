39365f0704e6fe00a2bdadc583b1c1883ee48900
Dmitry Sokolov <131367242+BYSOkoL@users.noreply.github.com>
2023-04-26 21:48:08 +0300

    push+

--- src/Les2.java
+++ src/Les2.java
@@ -3,38 +3,15 @@ import java.sql.SQLOutput;
 public class Les2 {
     public static void main(String[] args) {
 
-        String message = "Hello Java!";
-        System.out.println(message);
+        int number = 26556;
 
-        Boolean bool = false;
-        System.out.println(bool);
 
-        byte byteNumber = 10;
-        System.out.println(byteNumber);
 
-        short shortNumber = 1000;
-        System.out.println(shortNumber);
 
-        int intNumber = 123456;
-        System.out.println(intNumber);
+        int number3 = (number % 1000) / 100;
+        System.out.println(number3);
 
-        long longNumber = 1231111111111L;
-        System.out.println(longNumber);
 
-        //char symbol = 'A';
-        char symbol = 65;
-        System.out.println(symbol);
-
-        float floatNumber = 10.5f;
-        System.out.println(floatNumber);
-
-        double doubleNumber = 10.5;
-        System.out.println(doubleNumber);
-
-        //<< деление на 2
-        //>> умнож на 2
-
-        System.out.println(10 >> 1);
 
 
 
