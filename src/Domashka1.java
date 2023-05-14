40ca1ed83c9063b1cc806f12e4b6c3fa7981af9b
BYSOkoL <so-kol94@mail.ru>
2023-05-14 21:57:01 +0300

    zadanie1 ne redact

--- src/Domashka1.java
+++ src/Domashka1.java
@@ -1,17 +1,34 @@
 public class Domashka1 {
     public static void main(String[] args) {
 
-        int number = 26556;
+        int d0 = 5; //dlina uchastka
+        int sh0 = 6; //shirina uchastka
+        int d1 = 1; //Dlina pervogo
+        int sh1 = 6; //SHirina pervogo
+        int d2 = 1; //Dlina vtorogo
+        int sh2 = 1; //SHirina vtorogo
 
-
-
-
-        int number3 = (number % 1000) / 100;
-        System.out.println(number3);
-
-
-
+        if ((d0 >= (d1 + d2)) && (sh0 >= sh1 && sh0 >= sh2)) {
+            System.out.println("Can");
+        } else if ((d0 >= (d1 + sh2)) && (sh0 >= sh1 && sh0 >= d2)) {
+            System.out.println("Can");
+        } else if ((d0 >= (sh1 + d2)) && (sh0 >= d1 && sh0 >= sh2)) {
+            System.out.println("Can");
+        } else if ((d0 >= (sh1 + sh2)) && (sh0 >= d1 && sh0 >= d2)) {
+            System.out.println("Can");
+        } else if ((sh0 >= (d1 + d2)) && (d0 >= sh1 && d0 >= sh2)) {
+            System.out.println("Can");
+        } else if ((sh0 >= (d1 + sh2)) && (d0 >= sh1 && d0 >= d2)) {
+            System.out.println("Can");
+        } else if ((sh0 >= (sh1 + d2)) && (d0 >= d1 && d0 >= sh2)) {
+            System.out.println("Can");
+        } else if ((sh0 >= (sh1 + sh2)) && (d0 >= d1 && d0 >= d2)) {
+            System.out.println("Can");
+        } else
+            System.out.println("Нельзя построить 2 дома");
+    }
+}
 
 
     }
-}
+            }
