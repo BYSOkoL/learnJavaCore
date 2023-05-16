f43621b651c1fa3d4a5984ddd6e2bca0e1b06b13
BYSOkoL <so-kol94@mail.ru>
2023-05-16 19:05:25 +0300

    .

--- src/Task2.java
+++ src/Task2.java
@@ -2,23 +2,29 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        int needIndex = 12;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("index?");
+        int needIndex = scanner.nextInt();
+
         int index = 1;
         int firstDigit = 0;
         int secondDigit = 1;
-        int temp = 0;
+        int c = 0;
         int summ = 0;
-       // System.out.println(0);
-        while (index<=needIndex-1) {
-            if (secondDigit%2==0){
+        System.out.println(0);
+        while (index <= needIndex - 1) {
+            if (secondDigit % 2 == 0) {
                 summ += secondDigit;
             }
-          //  System.out.println(secondDigit);
-            temp = secondDigit;
+            System.out.println(secondDigit);
+            c = secondDigit;
             secondDigit = firstDigit + secondDigit;
-            firstDigit = temp;
+            firstDigit = c;
             index++;
+
         }
         System.out.println(summ);
+
+
     }
 }
