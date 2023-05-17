8ea2cb3ab46d1f302f82c6729cf7d96dbe9c6318
Dmitry Sokolov <131367242+BYSOkoL@users.noreply.github.com>
2023-05-17 19:17:05 +0300

    zvezdochkaotmena

--- src/Les.java
+++ src/Les.java
@@ -2,7 +2,19 @@ import java.util.Arrays;
 
 public class Les {
     public static void main(String[] args) {
-
-        
+        int[] array = {1, 2, 3, 4, 5, 6, 10, 7, 8, 9};
+        int max = array[0];
+        int min = array[0];
+        int indexMax = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                indexMax = i;
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        array[indexMax] = max * min;
     }
 }
--- src/TaskZvesdochka1.java
+++ src/TaskZvesdochka1.java
@@ -1,20 +1,6 @@
 public class TaskZvesdochka1 {
     public static void main(String[] args) {
-        int[] array = new int[]{1, 1, 1, 1};
-        int summ1 = 0;
-        int summ2 = 0;
-        for (int i = 1; i < array.length; i += 2) {
-            summ1 += array[i];
-        }
-        for (int i = 0; i < array.length; i += 2) {
-            summ2 += array[i];
-        }
-        if (summ1 < summ2) {
-            System.out.println(summ2);
-        } else if (summ1 > summ2) {
-            System.out.println(summ1);
-        } else {
-            System.out.println("summ1 = summ2 = " + summ1);
+
         }
     }
 }
