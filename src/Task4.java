14b403e5ba53adbe5694ee290c151fb7da4d1ff3
Dmitry Sokolov <131367242+BYSOkoL@users.noreply.github.com>
2023-05-17 13:26:48 +0300

    task4

---
+++ src/Task4.java
@@ -0,0 +1,23 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int[][] array = new int[][] {{5,7,-3,-17}, {-7,-3,-1,12}, {8,-1,2,3}};
+        int summPolozhit = 0;
+        int summOtric = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] < 0) {
+                    summOtric += 1;
+                } else {
+                    summPolozhit += 1;
+                }
+            }
+        }
+        if (summPolozhit > summOtric) {
+            System.out.println("Polozhitel'nih bolshe");
+        } else if (summOtric > summPolozhit) {
+            System.out.println("Otricatel'nih bolshe");
+        } else {
+            System.out.println("Porovnu");
+        }
+    }
+}
