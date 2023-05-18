16ad1444fd01630b78536391afb4ba81adfb8b1e
Dmitry Sokolov <131367242+BYSOkoL@users.noreply.github.com>
2023-05-18 12:24:31 +0300

    task2biginteger

---
+++ src/dmitry/sokolov/homework/first/Task2BigInteger.java
@@ -0,0 +1,26 @@
+package dmitry.sokolov.homework.first;
+
+import java.math.BigInteger;
+
+public class Task2BigInteger {
+    public static void main(String[] args) {
+        int index = 1;
+        int needIndex = 12;
+        BigInteger firstDigit = new BigInteger("0");
+        BigInteger secondDigit = new BigInteger("1");
+        BigInteger temp = new BigInteger("0");
+        BigInteger summ = new BigInteger("0");
+        while (index <= needIndex - 1) {
+            BigInteger chetnoeProverka = secondDigit.mod(BigInteger.valueOf(2));
+            int chetnoeProverkaInt = chetnoeProverka.intValue();
+            if (chetnoeProverkaInt == 0) {
+                summ = summ.add(secondDigit);
+            }
+            temp = secondDigit;
+            secondDigit = firstDigit.add(secondDigit);
+            firstDigit = temp;
+            index++;
+        }
+        System.out.println(summ);
+    }
+}
--- src/dmitry/sokolov/test/Main.java
+++ src/dmitry/sokolov/test/Main.java
@@ -5,10 +5,10 @@ import dmitry.sokolov.test.Box;
 public class Main {
     public static void main(String[] args) {
         Box [] boxes = {
-                new Box(1, 2, 3)
-                new Box(1, 2, 3)
-                new Box(1, 2, 3)
-                new Box(1, 2, 3)
+                new Box(1, 2, 3),
+                new Box(1, 2, 3),
+                new Box(1, 2, 3),
+                new Box(1, 2, 3),
         };
         int maxVolume = 0;
         for (Box box:
