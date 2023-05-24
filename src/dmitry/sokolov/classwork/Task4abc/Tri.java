5e69e66fa5242adc057006c49d67e1b2860dc2c2
Dmitry Sokolov <131367242+BYSOkoL@users.noreply.github.com>
2023-05-24 21:26:23 +0300

    cw

--- src/dmitry/sokolov/classwork/Task4abc/Tri.java
+++ src/dmitry/sokolov/classwork/Task4abc/Tri.java
@@ -1,4 +1,25 @@
 package dmitry.sokolov.classwork.Task4abc;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Tri {
+    public static void main(String[] args) {
+        var text = "12321cv321dsf32sdff6sdf4";
+        var pattern = Pattern.compile("\\d+");
+        var matcher = pattern.matcher(text);
+        Integer result = null;
+        while   (matcher.find()) {
+            int length = matcher.end() - matcher.start();
+            if (result == null || length > result) {
+                result = length;
+            }
+        }
+        if (result == null)
+        {
+            System.out.println("NO");
+        } else {
+            System.out.println("longest:" + result);
+        }
+    }
 }
